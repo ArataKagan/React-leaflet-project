f746dd0fb13ef0dcc50104ae11ff6e497376f321
Arata Kagan <aratakagan@macbook-air-5.lan>
2023-07-04 00:00:20 -0700

    import react-leaflet and added basic map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import React from "react";
+import { MapContainer, TileLayer } from 'react-leaflet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <MapContainer center={[35.689487, 139.691711]} zoom={12}>
+    <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    />
+  </MapContainer>
 }
 
-export default App;
+
